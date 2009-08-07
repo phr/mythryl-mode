386217bb073b9ae3e9c00726f45882a4ff4a17f8
Phil Rand <philrand@gmail.com>
2009-08-07 12:03:11 -0700

    Added COPYING, mythryl-mode.el with initial comments only.

---
+++ mythryl-mode.el
@@ -0,0 +1,25 @@
+;;; mythryl-mode.el --- mode for editing Mythryl code
+
+;; Copyright (C) 2009 Phil Rand <philrand@gmail.com>
+
+;; Mythryl-mode is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; mythryl-mode is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with mythryl-mode; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;; A major mode for editing the Mythryl programming language.
+
+;;; Code:
+
